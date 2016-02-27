2b6193de07763ea9075596292a135bcc513c48b5
Home <Home@armdev.ubuntu.com>
2016-02-27 12:24:50 +0400

    java 8 examples maven project

---
+++ vsjava8/src/main/java/com/lambda/Example.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Home
+ */
+public class Example {
+     public static void main(String args[]){
+      List names = new ArrayList();
+		
+      names.add("Mahesh");
+      names.add("Suresh");
+      names.add("Ramesh");
+      names.add("Naresh");
+      names.add("Kalpesh");
+		
+      names.forEach(System.out::println);
+   }
+}
